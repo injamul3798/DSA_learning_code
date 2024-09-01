b3d0d9bb3aab684007e376f549624b64ef6fbb86
Injamul Haque <injamul15-3798@diu.edu.bd>
2024-09-01 22:33:46 +0600

    Stack using list

---
+++ stack_mplementation using list.py
@@ -0,0 +1,20 @@
+stack_list = []
+
+#Stack consists of two operation
+# Push & Pop 
+# It is called LIFO =  Last in Fast out
+
+# Now lets push some element in the list
+stack_list.append('injam')
+stack_list.append('najmus')
+stack_list.append('c')
+stack_list.append(1)
+print("Print result before any pop operation: ")
+print(stack_list)
+
+# Now lets do pop operation
+stack_list.pop()
+stack_list.pop()
+print("Print result after any pop operation: ")
+print(stack_list)
+

1ed5dac8dfe562dd6757b4e33bb046f1efada081
Injamul <injamul15-3798@diu.edu.bd>
2024-09-09 11:24:11 +0600

    Create stack_using_deque.py

---
+++ stack_using_deque.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+stack = deque()
+
+stack.append('Injam')
+stack.append('Najmus')
+stack.append('Shakil')
+
+print("Stack List : ",stack)
+
+stack.pop()
+
+print("Stack List : ",stack)
+

665d5ab11709a96c61fa13bd2ad3319166f04ec6
Injamul <injamul15-3798@diu.edu.bd>
2024-09-09 11:41:44 +0600

    Update stack_using_deque.py

--- stack_using_deque.py
+++ stack_using_deque.py
@@ -1,14 +1,34 @@
+# check the stack is empty or not 
+def is_empty(stack):
+    return len(stack) == 0
+
+# get the first element
+def top_element(stack):
+    return stack[0]
+
+
 from collections import deque
 
 stack = deque()
 
+
+# append element into stack 
 stack.append('Injam')
 stack.append('Najmus')
 stack.append('Shakil')
 
 print("Stack List : ",stack)
 
+
+
+# Pop element from the list
+stack.pop()
 stack.pop()
 
 print("Stack List : ",stack)
+
+print('Stack is empty: ',is_empty(stack))
+print('Top element: ',top_element(stack))
+
+
 

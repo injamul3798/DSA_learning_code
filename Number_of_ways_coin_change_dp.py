572bff3b2a9583b1744817b4f6b34c2d030bbca1
Injamul Haque <injamul15-3798@diu.edu.bd>
2024-10-01 21:33:17 +0600

    Update Number_of_ways_coin_change_dp.py

--- Number_of_ways_coin_change_dp.py
+++ Number_of_ways_coin_change_dp.py
@@ -1,23 +1,27 @@
-target = 8
-coins = [1,3,5]
-len_of_coins = len(coins)
+coins = [1,2,5]
+target = 5
 
-dp = [[0 for _ in range(target + 1)] for _ in range(len_of_coins + 1)]
+dp = [[0 for _ in range(target+1)]for _ in range(len(coins)+1)]
 
-for i in range(0,len_of_coins+1):
-    for j in range(0,target + 1):
-        if i ==0 and j ==0:
-            dp[i][j] = 1
-        elif i == 0:
+for i in range(len(coins)+1):
+    for j in range(target+1):
+        if i==0 and j ==0:
             dp[i][j] = 0
+        elif j==0:
+            dp[i][j] = 1
         elif coins[i-1] > j:
             dp[i][j] = dp[i-1][j]
         else:
             dp[i][j] = dp[i-1][j] + dp[i][j-coins[i-1]]
-print(dp[len_of_coins][target])
+        
+print('Total Number of ways to make target: ',dp[len(coins)][target])
 
+print("Show the table: ")
 
-for i in range(0, len_of_coins + 1):
-    for j in range(0, target + 1):
-        print(dp[i][j], end=' ')   
-    print()   
+for i in range(len(coins)+1):
+    for j in range(target+1):
+        print(dp[i][j],end = ' ')
+    print()
+        
+        
+        

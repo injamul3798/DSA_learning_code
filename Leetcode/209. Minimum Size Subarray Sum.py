b501f1060a4412112779d5e22a0458629929be64
Injamul Haque <injamul15-3798@diu.edu.bd>
2024-10-21 21:58:13 +0600

    Sliding window

---
+++ Leetcode/209. Minimum Size Subarray Sum.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+        mn = float('inf')
+        left = 0
+        c_sum = 0
+        for item  in range(len(nums)):
+            c_sum += nums[item]
+            while(c_sum>= target):
+                mn = min(mn,item-left + 1)
+                c_sum -= nums[left]
+                left += 1
+
+        if mn != float('inf'):
+            return mn
+        else:
+            return 0
+            
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+        

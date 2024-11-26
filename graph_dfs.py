7047ad047d53633e9b31c482786aa78766ee0463
Injamul Haque <injamul15-3798@diu.edu.bd>
2024-11-26 15:20:14 +0600

    Create graph_dfs.py

---
+++ graph_dfs.py
@@ -0,0 +1,21 @@
+def recursive_dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(node)
+    for neighbor in graph[node]:
+        if neighbor not in visited:
+            recursive_dfs(graph, neighbor, visited)
+    return visited
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# Start DFS from node 'A'
+visited_nodes = recursive_dfs(graph, 'A')
+print(visited_nodes)

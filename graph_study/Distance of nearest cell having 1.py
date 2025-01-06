bbcf380df6730e0817b43eba6e4d4d08c9c7e626
Injamul Haque <injamul15-3798@diu.edu.bd>
2025-01-06 17:02:41 +0600

    0 1 Matrix

---
+++ graph_study/Distance of nearest cell having 1.py
@@ -0,0 +1,42 @@
+from collections import deque
+class Solution:
+    def distanceNearestCell(self,grid):
+        m = len(grid)
+        n = len(grid[0])
+        queue = deque()
+        visited = set()
+        
+        distance = []
+        for i in range(m):
+            row = []
+            for j in range(n):
+                row.append(None)
+            distance.append(row)
+            
+        # Now lets insert those element which value is 1
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1:
+                    queue.append(((i,j),0))
+                    visited.add((i,j))
+                    
+        while queue:
+            cell,steps = queue.popleft()
+            nr,nc = cell
+            distance[nr][nc] = steps
+            
+            for i,j in [(-1,0),(0,-1),(0,1),(1,0)]:
+                newr = nr + i
+                newc = nc + j
+                
+                if (newr>=0 and newr<m) and (newc>=0 and newc<n) and (grid[newr][newc]==0) and (newr,newc) not in  visited:
+                    queue.append(((newr,newc),steps+1))
+                    visited.add((newr,newc))
+                
+        return distance
+                    
+                    
+if __name__ == '__main__':
+    grid = [[0,1,1,0], [1,1,0,0], [0,0,1,1]]
+    ob = Solution()
+    print(ob.distanceNearestCell(grid))

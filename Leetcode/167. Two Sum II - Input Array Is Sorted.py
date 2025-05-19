477854bc62c809e75b05db7799cfdc548a88fee2
Injamul Haque <101572467+injamul3798@users.noreply.github.com>
2025-05-19 21:58:39 +0600

    Update 167. Two Sum II - Input Array Is Sorted.py

--- Leetcode/167. Two Sum II - Input Array Is Sorted.py
+++ Leetcode/167. Two Sum II - Input Array Is Sorted.py
@@ -1,19 +1,27 @@
-class Solution(object):
-    def twoSum(self, numbers, target):
-        """
-        :type numbers: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        hash_map = {}
-        for i in range(len(numbers)):
-            hash_map[numbers[i]] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        for i in range(len(numbers)):
-            temp = target - numbers[i]
-            if temp in hash_map and hash_map[temp] > i:
-                return [i+1,hash_map[temp]+1]
+        # Solution 01
+        # hash_map = {}
+        # for i in range(len(numbers)):
+        #     hash_map[numbers[i]] = i
+        
+        # for i in range(len(numbers)):
+        #     need = target - numbers[i]
+        #     if need in hash_map and hash_map[need] > i:
+        #         return [i+1,hash_map[need]+1]
 
-        return -1
+
+        # Solutions 02
+        left,right = 0,len(nums)-1
+        while left <right:
+            current = nums[left] + nums[right]
+            if current > target:
+                right -= 1
+            elif current < target:
+                left += 1
+            else:
+                return [left+1,right+1]
+
 
         
